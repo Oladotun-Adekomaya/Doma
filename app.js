ffe0e8e06152936c4af7c022bc5a699675b2789a
Oladotun Adekomaya <oladotunadekomaya@gmail.com>
2022-07-12 22:31:07 +0100

    added remote db

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const User = require('./models/user')
 
 const MongoStore = require('connect-mongo');
 
-// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/doma';
-const dbUrl = 'mongodb://localhost:27017/doma';
+const dbUrl = process.env.DB_URL || //'mongodb://localhost:27017/doma';
+//const dbUrl = 'mongodb://localhost:27017/doma';
 
 mongoose.connect( dbUrl ,{
     useNewUrlParser: true, 
